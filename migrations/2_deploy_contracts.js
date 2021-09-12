b499c7264728b3ad1fc058cdfad613d79ff26d13
julius.suominen <julius.suominen@gmail.com>
2021-09-12 19:47:02 +0300

    add ContextSwitch, .call & .delegatecall

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const Adoption = artifacts.require("./Adoption.sol");
 const BaseCaller = artifacts.require("./BaseCaller.sol");
+const ContextSwitcher = artifacts.require("./ContextSwitcher.sol");
 
 module.exports = async function(deployer) {
   await deployer.deploy(Adoption);
   await deployer.deploy(BaseCaller);
+  await deployer.deploy(ContextSwitcher);
 };
